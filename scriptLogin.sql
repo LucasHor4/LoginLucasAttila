91fdca778dc0816ce5924b2cb86818189bb7c305
LucasHor4 <lucash.arcanjo267@gmail.com>
2025-10-10 11:50:46 -0300

    .

--- scriptLogin.sql
+++ scriptLogin.sql
@@ -3,23 +3,24 @@ create database dbLogin;
 use dbLogin;
 
 create table tbCliente(
-Id int primary key, 
+Id int auto_increment primary key, 
 Nome varchar(50) not null,
-Nasc date not null,
+Nasc datetime not null,
 Sexo char(1) not null,
-Senha varchar(50) not null,
-CPF bigint not null,
-Telefone varchar(13) not null,
+Senha varchar(8) not null,
+ConfirmacaoSenha varchar(8) not null,
+CPF varchar(11) not null,
+Telefone varchar(14) not null,
 Email varchar(50) not null,
-Situacao varchar (50) not null
+Situacao char(1) not null
 );
 
 create table tbColaborador(
-Id int primary key, 
+Id int auto_increment primary key, 
 Nome varchar(50) not null,
-Senha varchar(50) not null,
+Senha varchar(8) not null,
 Email varchar(50) not null,
-Tipo varchar (50) not null
+Tipo varchar (8) not null
 );
 
 select * from tbCliente;
