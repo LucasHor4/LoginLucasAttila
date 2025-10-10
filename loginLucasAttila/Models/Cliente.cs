d15b7b88428bcfa8cbdec12bab02718faafe9d03
LucasHor4 <lucash.arcanjo267@gmail.com>
2025-10-10 12:19:15 -0300

    Continuando Projeto-com o luva

--- loginLucasAttila/Models/Cliente.cs
+++ loginLucasAttila/Models/Cliente.cs
@@ -5,31 +5,43 @@ namespace loginLucasAttila.Models
 {
     public class Cliente
     {
-        [Display(Name = "Código.", Description = "Código.")]
+        /* PK */
+        [Display(Name = "Código", Description = "Código.")]
         public int Id { get; set; }
 
-        [Display (Name = "Nome completo.", Description = "Nome completo.")]
+        [Display(Name = "Nome completo", Description = "Nome e Sobrenome.")]
+        [Required(ErrorMessage = "O nome completo é obrigatório.")]
         public string Nome { get; set; }
-        
-        [Display (Name = "Data de nascimento.", Description = "Data de nascimento.")]
-        public DateOnly Nasc {  get; set; }
-        
-        [Display (Name = "Sexo.", Description = "Sexo.")]
+
+        [Display(Name = "Nascimento")]
+        [Required(ErrorMessage = "A data é obrigatória")]
+        public DateTime Nascimento { get; set; }
+
+        [Display(Name = "Sexo")]
+        [Required(ErrorMessage = "Selecione uma opção")]
+        [StringLength(1, ErrorMessage = "Deve conter 1 caracter")]
         public string Sexo { get; set; }
-        
-        [Display (Name = "Senha.", Description = "Senha.")]
-        public string Senha { get; set; }
-                
-        [Display (Name = "CPF.", Description = "CPF.")]
+
+        [Display(Name = "CPF")]
+        [Required(ErrorMessage = "O CPF é obrigatório")]
         public string CPF { get; set; }
 
-        [Display (Name = "Telefone.", Description = "Telefone.")]
-        public int Telefone { get; set; }
+        [Display(Name = "Celular")]
+        [Required(ErrorMessage = "O Celular é obrigatório")]
+        public string Telefone { get; set; }
 
-        [Display (Name = "Email completo.", Description = "Email completo.")]
+        [Display(Name = "Email")]
+        [EmailAddress(ErrorMessage = " O Email não é valido")]
+        [RegularExpression(".+\\@.+\\..+", ErrorMessage = "Informe um e-mail válido...")]
         public string Email { get; set; }
 
-        [Display (Name = "Situação.", Description = "Situação.")]
-        public string Situacao { get; set; }
+        [Display(Name = "Senha")]
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "O senha é obrigatório")]
+        [StringLength(10, MinimumLength = 6, ErrorMessage = "A senha deve ter entre 6 e 10 caracteres")]
+        public string Senha { get; set; }
+
+        [Display(Name = "Situação")]
+        public string? Situacao { get; set; }
     }
 }
---
+++ loginLucasAttila/Repository/Contract/IClienteRepository.cs
@@ -0,0 +1,27 @@
+﻿using loginLucasAttila.Models;
+
+namespace loginLucasAttila.Repository.Contract
+{
+    public interface IClienteRepository
+    {
+        //login Cliente
+
+        Cliente Login(string Email , string Senha);
+
+        //CRUD
+
+        void Cadastrar(Cliente cliente);
+        void Atualizar(Cliente cliente);
+        
+        void Ativar(int id);
+        void Desativar(int id);
+        void Excluir(int id);
+
+        Cliente ObterCliente(int  id);
+        Cliente BuscaCpfCliente(string CPF);
+        Cliente BuscaEmailCliente(string email);
+
+        IEnumerable<Cliente> ObterTodosClientes();
+
+    }
+}
